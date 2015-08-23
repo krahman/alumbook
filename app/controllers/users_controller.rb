0a0a2703344096580bf6fc989526ceefb391184d
Khalilur Rahman <khal.rahman@gmail.com>
2015-08-23 11:38:51 +0800

    added people types

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only:[:index]
 	before_action :set_user, only:[:show]
 	
 	def index
-		@users = User.all
-  	end
+    case params[:people] when "friends"
+      @users = current_user.active_friends
+    when "requests"
+      @users = current_user.pending_friend_request_from.map(&:user)
+    when "pending"
+      @users = current_user.pending_friend_request_to.map(&:friend)
+    else
+      @users = User.where.not(id: current_user.id)
+    end
+  end
 
   def show
   end
